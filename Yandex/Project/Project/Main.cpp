6d683df7de8707fccd40f001b6c7d949e6ebf129
aleks-sh <aleks-sh@mail.ru>
2019-07-12 14:38:43 +0300

    while 14

---
+++ Yandex/Project/Project/Main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int Fibonachy(int n);
+int NFibonachy(int f);
+int newSeries();
+
+int main() {
+	int n;
+	//cin >> n;
+	cout << newSeries();
+
+	cin.get();
+	cin.get();
+	return 0;
+}
+
+
+int Fibonachy(int n) {
+	int a = 0, b = 1, i = 0;
+	while (i < n) {
+		i++;
+		b = a + b;
+		a = b - a;
+	}
+	return a;
+
+}
+
+int NFibonachy(int f) {
+	int a = 0, b = 1, i = 0;
+	while (a < f) {
+		i++;
+		b = a + b;
+		a = b - a;
+	}
+	return (a == f) ? i : -1;
+}
+
+int newSeries() {
+	int number = 0, prevNumber = 0;
+	int series = 1, maxSeries = 1;
+	do
+	{
+		cin >> number;
+		if (prevNumber == number) series++;
+		else {
+			if (maxSeries < series) maxSeries = series;
+			series = 1;
+		}
+		prevNumber = number;
+	} while (number!=0);
+	
+	return maxSeries;
+}
